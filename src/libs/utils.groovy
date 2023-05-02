099f523c5fc323951cf286db0d38a64bd9fb141b
Romas <Romas>
2023-05-02 17:16:52 +0300

    Added name parameter esixtence check

--- src/libs/utils.groovy
+++ src/libs/utils.groovy
@@ -1,6 +1,9 @@
 package libs;
 
 def helloWorld(Map params){
+    if (params.name == null){
+        throw new Exception("Name parameter is missing!!!!!");
+    }
     sh "echo Hello ${params.name}. This is ${params.build_id} build"
 }
 
